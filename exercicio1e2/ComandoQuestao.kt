9e4082873cbd3f1136bab2d8d19426e7a24e2a13
kimalextst <kimtorresst@gmail.com>
2022-05-14 12:23:26 -0300

    .

---
+++ exercicio1e2/ComandoQuestao.kt
@@ -0,0 +1,8 @@
+package exercicio1e2
+
+/*1. Definir e inicializar uma nova lista vazia. Adicionar os seguintes
+elementos à lista vazia: [1,5,5,6,7,8,8,8]. Imprimir o resultado na tela.
+
+2. Definir e inicializar um novo conjunto vazio. Adicionar os seguintes
+elementos ao conjunto vazio: {1,5,5,6,7,8,8,8}. Imprimir o resultado na
+tela. Qual é a diferença entre este exercício e o anterior?*/
